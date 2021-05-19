f87e5a419cbcf63ec40ee912eba8121085612a00
Dmitrii <Adept1213@gmail.com>
2021-05-19 15:39:58 +0400

    19.05

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,10 +1,5 @@
-1. Сделать перечеркивание при нажтии на Staric tr
+ok 1. Сделать перечеркивание при нажтии на Staric tr
 2. Ограничить input count and price, type Numbers
-3. TotalPrice
-4. count products taken
+ok 3. TotalPrice
+ok 4. count products taken
 
-
-"<input type='text' class='name' value='"+ name +"' placeholder='name' onChange='changeState("+id+", `name`, value)' >" +    
-                        "<input type='text' class='count' value='"+ count +"' placeholder='count' onChange='changeState("+id+", `count`, value)'>" +
-                        "<input type='text' class='price' value='"+ price +"' placeholder='price' onChange='changeState("+id+", `price`, value)'>" +
-                        "<th class='totalCost'>" + totalCost + "</th>";
--- Script.js
+++ Script.js
@@ -1,13 +1,15 @@
-
-
+const BOUGHT = 'bought';
+const NEED_BUY = 'needBought';
+const CLASS_CROSSOUT = 'crossOut';
 
 //store
 let store = [
     list05052021 = [
-        {id : 0, name : 'Banana', count : '1', price: '65',  }, 
-        {id : 1, name : 'Fruit', count : '0.8', price: '80',},
-        {id : 2, name : 'Bread', count : '3', price: '34', },
-        {id : 3, name : 'Milk', count : '2', price: '54',  },
+        {id : 0, totalCost: 0, totalPrice: 0},
+        {id : 1, name : 'Banana', count : '1', price: '65', type: 'needByu', }, 
+        {id : 2, name : 'Fruit', count : '0.8', price: '80', type: 'needByu',},
+        {id : 3, name : 'Bread', count : '3', price: '34', type: 'needByu',},
+        {id : 4, name : 'Milk', count : '2', price: '54', type: 'needByu', },
     ],
 ]
 
@@ -17,12 +19,8 @@ function changeState (id, className, value) {
 }
 
 //-------------------------------------------------------------------------------------
-
-
-
-
 //create productLine change
-function productLineChange(id, name, count, price, totalCost){
+function productLineChange(id, name, count, price, totalCost, type){
     let product = document.createElement('tr'); // create tr
     product.classList.add(id);            // add class
 
@@ -32,11 +30,13 @@ function productLineChange(id, name, count, price, totalCost){
                     creatInput(id, 'price', price),);
     product.innerHTML += "<th class='totalCost'>" + totalCost + "</th>";
     
+    makeBought_needBuy(type, product);
+
     let main = document.querySelector('.products'); 
     main.append(product); 
 };
 
-//create input th
+//create input 
 function creatInput(id, className, productStateValue) {
     let creatInput = document.createElement('input');
     creatInput.classList.add(className);
@@ -45,10 +45,30 @@ function creatInput(id, className, productStateValue) {
     return creatInput;
 }
 
+//change store and render
+function giveFuncInput () { 
+    let allInput = document.querySelectorAll('input');
+    for ( let i = 0; i < allInput.length; i++) {
+        allInput[i].onchange = () => {
+            changeState ( allInput[i].parentNode.classList,
+                            allInput[i].className,
+                            allInput[i].value);
+            render(productLineChange);
+        }    
+    }
+}
+
+function makeBought_needBuy (type, product) {
+    type == BOUGHT ? 
+    product.classList.add(CLASS_CROSSOUT):
+    product.classList.remove(CLASS_CROSSOUT);
+}
+
+
 
 
 //create productLine static
-function productLineStatic(id, name, count, price, totalCost){
+function productLineStatic(id, name, count, price, totalCost, type){
 
     let product = document.createElement('tr');
     product.classList.add(id);
@@ -57,12 +77,31 @@ function productLineStatic(id, name, count, price, totalCost){
                         "<th class='price'>" + price + "</th>" +
                         "<th class='totalCost'>" + totalCost + "</th>";
 
-    //needBuy Bought
-    product.onclick = () => product.classList.toggle('crossOut');
+    giveFuncDiv(id, type, product);
 
     let main = document.querySelector('.products');
     main.append(product);
 };
+
+//give func change type ' needBuy, bought', add remove class crossOut, render
+function giveFuncDiv(id, type, product){
+
+    if(type == 'bought') product.classList.add('crossOut');  
+
+    product.onclick = () => {
+        if(product.classList == `${id}`) {
+            changeState(id, 'type', 'bought');
+            render(productLineStatic);
+        } else {
+            changeState(id, 'type', 'needBuy');
+            render(productLineStatic);        
+        }
+    }
+}
+
+
+
+
 //----------------------------------------------------------------------------
 
 
@@ -93,12 +132,38 @@ buttonAdd.onclick = () => {
     let classButtonChange = 'buttonChange';
     let classButtonDone = 'buttonDone';
 
-    store[0][store[0].length] = {id: ++store[0].length, name: '', count: '', price: ''}
+    store[0][store[0].length] = {id: store[0].length, name: '', count: '', price: '', type: 'needByu',}
     buttonChangeDone.classList.remove(classButtonChange);
     buttonChangeDone.classList.add(classButtonDone);
     buttonChangeDone.innerHTML = 'Done';
+    console.log(store[0]);
     render(productLineChange);
 }
+
+//----------------------------------------------------------------------------
+//show totalPrice
+function showTotalPrice () {
+    let totalPrice = 0;
+    for (let i = 1; i < store[0].length; i++){
+        totalPrice += store[0][i].price * store[0][i].count;
+    }
+    changeState(0, 'totalPrice', totalPrice)
+    document.querySelector('.header__totalCost__price').innerHTML = store[0][0].totalPrice;
+}
+
+function showTotalCost () {
+    let totalCost = 0;
+    for (let i = 1; i < store[0].length; i++){
+        if(store[0][i].type == BOUGHT){
+            totalCost += store[0][i].price * store[0][i].count;
+        }
+    }
+    changeState(0, 'totalCost', totalCost)
+    document.querySelector('.header__totalCost__cost').innerHTML = store[0][0].totalCost;
+}
+
+
+
 
 //---------------------------------------------------------------------------------
 //render
@@ -110,25 +175,17 @@ function render (boo) {
     //render
     for (let i = 0; i < store.length; i++){
         for ( let x in store[i]){
+            if(store[i][x].id == 0) continue;
             let totalCost = store[i][x].count * store[i][x].price;
-            boo(store[i][x].id, store[i][x].name, store[i][x].count, store[i][x].price, totalCost);          
+            boo(store[i][x].id, store[i][x].name, store[i][x].count, store[i][x].price, totalCost, store[i][x].type);          
         }       
     }
     giveFuncInput();
+    showTotalPrice();
+    showTotalCost();
 } 
 
 
-//change store and render
-function giveFuncInput () { 
-    let allInput = document.querySelectorAll('input');
-    for ( let i = 0; i < allInput.length; i++) {
-        allInput[i].onchange = () => {
-            changeState ( allInput[i].parentNode.classList,
-                            allInput[i].className,
-                            allInput[i].value);
-            render(productLineChange);
-        }    
-    }
-}
+
 
 render(productLineStatic);
